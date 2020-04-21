108fcbf4c47c819d16cf4f45237dc3dcf8246679
dnamobile <dnamobile@users.noreply.github.com>
2020-04-21 20:29:44 -0300

    Correção de bug Webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  }))
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
 module.exports = environment
