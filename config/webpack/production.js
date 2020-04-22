74346753aae9eebdc88991cc9eb6d30335a6c123
dnamobile <dnamobile@users.noreply.github.com>
2020-04-22 19:19:35 -0300

    ajuste de script

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,24 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-{
-    test: /\.(scss)$/,
-    use [{
-      loader: 'style-loader', // inject CSS to page
-    }, {
-      loader: 'css-loader', // translates CSS into CommonJS modules
-    }, {
-      loader: 'postcss-loader', // Run post css actions
-      options: {
-        plugins: function () { // post css plugins, can be exported to postcss.config.js
-          return [
-            require('precss'),
-            require('autoprefixer')
-          ];
-        }
-      }
-    }, {
-      loader: 'sass-loader' // compiles Sass to CSS
-    }]
-}
