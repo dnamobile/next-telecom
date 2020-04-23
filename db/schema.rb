36fa33548d81c235e43788f4ff9dda3993b5e43a
dnamobile <dnamobile@users.noreply.github.com>
2020-04-23 15:19:41 -0300

    Adicionando role ao Usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_010436) do
+ActiveRecord::Schema.define(version: 2020_04_23_180903) do
 
   create_table "bairros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "zona"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_010436) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
