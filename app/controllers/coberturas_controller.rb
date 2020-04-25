798e1e49bd6d4674ce9b4fc1839572d87179b5e9
dnamobile <dnamobile@users.noreply.github.com>
2020-04-25 00:46:26 -0300

    Ajustes de controller

--- app/controllers/coberturas_controller.rb
+++ app/controllers/coberturas_controller.rb
@@ -1,15 +1,16 @@
 class CoberturasController < ApplicationController
   before_action :authenticate_user!
-  
-  def set_field_classes
-    @field_classes = [Cobertura]    
-  end
 
-  def set_model_class
-    @model_class = Cobertura  
-  end
 
   private
+  
+    def set_field_classes
+      @field_classes = [Cobertura]    
+    end
+
+    def set_model_class
+      @model_class = Cobertura  
+    end
     def model_params
       params.require(:cobertura).permit(:endereco, :vivo, :net, :oi)
     end
--- app/controllers/enderecos_controller.rb
+++ app/controllers/enderecos_controller.rb
@@ -1,16 +1,29 @@
 class EnderecosController < ApplicationController
   before_action :authenticate_user!
 
-  def set_field_classes
-    @field_classes = []    
+  def new
+    @ref = @model_class.new
+    @ref.build_local
+    authorize @ref
   end
-
-  def set_model_class
-    @model_class = Endereco  
+  
+  def edit
+    if @ref.local.blank?
+      @ref.build_local
+    end
+    authorize @ref
   end
 
   private
-    
+  
+    def set_field_classes
+      @field_classes = []    
+    end
+
+    def set_model_class
+      @model_class = Endereco  
+    end
+  
     def model_params
       params.require(:endereco)
         .permit(:logradouro, :numero, :complemento, 
